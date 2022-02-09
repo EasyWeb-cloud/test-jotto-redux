9eb92e28978522fa6689371a1cd40110ab280282
Oleh Myhal <adriancraciunel1@gmail.com>
2022-02-09 21:38:28 +0200

    test reducer success reducer

---
+++ src/actions/successReducer.test.js
@@ -0,0 +1,17 @@
+import { actionTypes } from "./";
+import successReducer from "../reducers/successReducer";
+
+test('when previous state is undefined return false', () => {
+    const newState = successReducer(undefined, {})
+    expect(newState).toBe(false)
+})
+
+test('return prev state when unknown action type', () => {
+    const newState = successReducer(false, { type: 'unknown' })
+    expect(newState).toBe(false)
+})
+
+test('return true if correct action type', () => {
+    const newState = successReducer(false, { type: actionTypes.CORRECT_GUESS })
+    expect(newState).toBe(true)
+})
